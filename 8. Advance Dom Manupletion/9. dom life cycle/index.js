76f13966812c926a3d85daee582e995816efce4d
theteamisbest <abhithory@gmail.com>
2023-02-15 14:18:07 +0530

    dom life cycle and loading js script

---
+++ 8. Advance Dom Manupletion/9. dom life cycle/index.js
@@ -0,0 +1,18 @@
+"use strict"
+
+document.addEventListener("DOMContentLoaded",function(e){
+    console.log("html parsed and dom tree built!",e);
+})
+
+
+window.addEventListener("load",function(e){
+    console.log("page fully loaded",e);
+})
+
+
+window.addEventListener("beforeunload",function(e){
+    e.preventDefault();
+    console.log(e);
+
+    e.returnValue = "aaaa";
+})
