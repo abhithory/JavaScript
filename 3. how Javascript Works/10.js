312b48f20bdc4b9dee9f1ccdf315983794897295
theteamisbest <abhithory@gmail.com>
2023-06-13 14:23:13 +0530

    thread and asyncronous added

---
+++ 3. how Javascript Works/10.js
@@ -0,0 +1,13 @@
+// Js is a single threaded programming language
+
+// A thread in computer science refers to an individual sequence of instructions that can be executed independently within a process. It represents the smallest unit of work that can be scheduled by an operating system.
+
+// In a multi-threaded environment, multiple threads can exist within a single process, and these threads can execute concurrently. Each thread has its own program counter, stack, and local variables, but they share the same memory space, allowing them to communicate and share data with each other.
+
+// Now, let's discuss JavaScript's single-threaded nature. JavaScript is primarily single-threaded, meaning it executes one operation at a time in a single sequence. It uses an event-driven, non-blocking model, where tasks are added to an event queue and executed in a specific order.
+
+
+
+// Asynchronous refers to a programming paradigm or approach where tasks or operations are executed independently and concurrently, without waiting for each other to complete before moving on to the next task. In simple terms, it means that multiple operations can be initiated and run in parallel, allowing the program to continue its execution without being blocked.
+
+
